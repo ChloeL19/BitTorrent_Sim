7cbb2e18dd0f10339016ab72e2e877846281dd03
Chloe Loughridge <cloughridge@college.harvard.edu>
2021-09-21 14:11:11 -0400

    progress on reciprocation

--- clocstd.py
+++ clocstd.py
@@ -13,7 +13,7 @@ from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class Dummy(Peer):
+class ClocStd(Peer):
     def post_init(self):
         print(("post_init(): %s here!" % self.id))
         self.dummy_state = dict()
@@ -46,11 +46,31 @@ class Dummy(Peer):
 
         requests = []   # We'll put all the things we want here
         # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
+        #random.shuffle(needed_pieces)
+
+        # CHARLOTTE WORKING ON THIS
+        # maybe use the internal state for tracking this variable
+        # of the pieces we have, what is their availability among the other peers
+        # how many of the most rare pieces does each peer have
+        # sort peers by this
+        # av_dict = {} # count of other peers with this piece
+        # for np in np_set: 
+        #     peers_with_piece = 0
+        #     for p in peers:
+        #         if np in p.available_pieces:
+        #             peers_with_piece += 1
+        #     av_dict[np] = peers_with_piece
+        # # design a metric to rank peers by the number of rare pieces they have
+        # scored_peers = {}
+        # for p in peers:
+
+        # probably need to still think about breaking symmetries . . . randomize ties
         
         # Sort peers by id.  This is probably not a useful sort, but other 
         # sorts might be useful
         peers.sort(key=lambda p: p.id)
+        # change sorting to rarest first
+
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
         for peer in peers:
@@ -89,6 +109,30 @@ class Dummy(Peer):
         # has a list of Download objects for each Download to this peer in
         # the previous round.
 
+        # implement optimistic unchoking here --> is it just random?
+        # also implement reciprocation here: this is where we make assumptions about
+        # what makes a peer worth collaborting with?
+
+        # get upload speed estimates for all of the requesters
+        requester_rates = {} # r.peer_id -> pieces/round
+        for r in requests:
+            # use download rates as a proxy for upload rates
+            p_history = history.peer_history(r.peer_id)
+            # get the corresponding peer object
+            try:
+                peer = list(filter(lambda p: p.id == r.peer_id, peers))[0] #CONFIRM: okay to assume each peer ID only appears once in list?
+            except:
+                raise Exception("The requestor is not in the passed list of peers.")
+            # Q: should I exclude this round as part of estimation of download/upload rates
+            # units of this metric should be pieces/round
+            peer_uploadrate_proxy = len(set(peer.available_pieces))/p_history.current_round()
+            requester_rates[r.id] = peer_uploadrate_proxy
+        # sort the requesters by the value of their rates greatest --> least
+        # looking here will be helpful: https://careerkarma.com/blog/python-sort-a-dictionary-by-value/ 
+        requester_rates_sorted = sorted(requester_rates.items(), key=lambda x: x[1], \
+            reverse=True)
+        # General Q: how do we incrementally test this thing?????
+
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
             chosen = []
@@ -97,7 +141,7 @@ class Dummy(Peer):
             logging.debug("Still here: uploading to a random peer")
             # change my internal state for no reason
             self.dummy_state["cake"] = "pie"
-
+            # requester
             request = random.choice(requests)
             chosen = [request.requester_id]
             # Evenly "split" my upload bandwidth among the one chosen requester
